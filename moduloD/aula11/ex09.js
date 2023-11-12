be5be2e912cea84716f6af4b7b4d99534f17ed92
Isascaraber <isa.escaraber@gmail.com>
2023-11-12 20:13:06 -0300

    Feat: Add exercises about js and your properties

--- moduloD/aula11/ex09.js
+++ moduloD/aula11/ex09.js
@@ -1,7 +1,13 @@
-var país = `Brazil`;
-console.log(`Living in ${país}`);
-if (país == `Brazil`) {
-  console.log(`Brazilian`);
-} else {
-  console.log(`Foreigner`);
+function Verified() {
+  var country = window.document.querySelector("input#country").value; //Não pode esquecer o value no final quando é texto.
+  var res = window.document.querySelector("div#res");
+  res.innerHTML = `<p>Your country is <strong>${country}</strong></p>`;
+
+  if (country == `Brazil`) {
+    res.innerHTML += `<p>It means your are <strong/>Brazilian<strong/></p>`;
+    // console.log(`Brazilian`);
+  } else {
+    res.innerHTML += `<p>Oh, your are a <strong/>Foreigner<strong/>!</p>`;
+    // console.log(`Foreigner`);
+  }
 }
