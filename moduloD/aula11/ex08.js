6594c7a05e2ef07feb659fefd89e10e52e3f78fa
Isascaraber <isa.escaraber@gmail.com>
2023-11-12 19:08:14 -0300

    Feat: Add exercises about js and your properties

---
+++ moduloD/aula11/ex08.js
@@ -0,0 +1,8 @@
+// console.log("The console is working corretaly");
+var vel = 70;
+console.log(`The speed of your vehicle is ${vel}Km/h`);
+if (vel > 60) {
+  //Simple condition
+  console.log(`You exceeded the permitted speed. You will be fined.`);
+}
+console.log(`Always drive using your seat belt!`);
---
+++ moduloD/aula11/ex09.js
@@ -0,0 +1,7 @@
+var país = `Brazil`;
+console.log(`Living in ${país}`);
+if (país == `Brazil`) {
+  console.log(`Brazilia`);
+} else {
+  console.log(`Foreigner`);
+}
