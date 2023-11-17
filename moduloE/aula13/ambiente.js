43f6a55b4007289b881abbf0cf5719cfb5bba33c
Isascaraber <isa.escaraber@gmail.com>
2023-11-17 11:23:45 -0300

    Feat: Add new ex about repetition

---
+++ moduloE/aula13/ambiente.js
@@ -0,0 +1,23 @@
+var c = 1;
+do {
+  console.log(`step ${c}`);
+  c++;
+} while (c <= 6);
+
+/*
+var c = 1;
+while (c <= 6) {
+  console.log(`step ${c}`);
+  // console.log("How are you?");
+  c++; // c = c + 1
+}
+*/
+
+/*
+console.log("How are you?");
+console.log("How are you?");
+console.log("How are you?");
+console.log("How are you?");
+console.log("How are you?");
+console.log("How are you?");
+*/
