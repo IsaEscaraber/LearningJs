f5f6b3f8725029fcf510880f8c0381af43ca153a
Isascaraber <isa.escaraber@gmail.com>
2023-11-18 11:46:46 -0300

    Feat: Add the new folder of the class number 14

---
+++ moduloE/aula14/ambient.js
@@ -0,0 +1,16 @@
+// var c = 1;
+// while (c <= 5) {
+//   console.log(c);
+//   c++; //c = c + 1
+// }
+
+// do{
+//   console.log(c);
+//   c++; //c = c + 1
+// }while (c <= 5)
+
+console.log("It will start...");
+for (var c = 1; c <= 5; c++) {
+  console.log(c);
+}
+console.log("The end");
