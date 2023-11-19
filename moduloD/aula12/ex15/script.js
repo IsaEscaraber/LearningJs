37f5d95d9294b406cc182d86ff044516b2c57369
Isascaraber <isa.escaraber@gmail.com>
2023-11-19 12:25:56 -0300

    Feat: Adding new ex

--- moduloD/aula12/ex15/script.js
+++ moduloD/aula12/ex15/script.js
@@ -3,9 +3,9 @@ function verified() {
   var year = date.getFullYear();
   var fYear = document.getElementById("txtyear");
   var res = document.querySelector("div#res");
-  if (fYear.value.length == 0 || Number(fYear.value) > year)
+  if (fYear.value.length == 0 || Number(fYear.value) > year) {
     window.alert("Check the data and try again!");
-  else {
+  } else {
     var fgender = document.getElementsByName("radgender");
     var age = year - Number(fYear.value);
     // res.innerHTML = `Calculated age: ${age}`;
---
+++ moduloE/aula14/ex16/script.js
@@ -0,0 +1,11 @@
+function verified() {
+  var start = "";
+  var end = "";
+  var Step = Number(step.value);
+
+  if (Step <= 0) {
+    window.alert("teste");
+  } else {
+    window.alert("teste2");
+  }
+}
