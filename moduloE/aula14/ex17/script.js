c65a71890ccee7fee117d4ee97cf393e2e380b88
Isascaraber <isa.escaraber@gmail.com>
2023-11-22 06:43:17 -0300

    Feat: Adding a completion of the  ex 17 about table

--- moduloE/aula14/ex17/script.js
+++ moduloE/aula14/ex17/script.js
@@ -1,17 +1,26 @@
 function generatorTable() {
   let multiplicateNumber = document.getElementById("Multiplicate");
+  let tab = document.getElementById("selcTable");
   let res = document.getElementById("res");
 
   if (multiplicateNumber.value.length == 0) {
     window.alert("Error. Please enter a number");
   } else {
-    res.innerHTML = "Multiplicate Table: <br>";
     let stringToNumber = Number(multiplicateNumber.value);
-
+    tab.innerHTML = "";
+    // res.innerHTML = "Multiplicate Table: <br>";
     for (let count = 1; count <= 10; count++) {
-      res.innerHTML += `${count} x ${stringToNumber} = ${
-        count * stringToNumber
-      } <br />`;
+      // res.innerHTML += `${count} x ${stringToNumber} = ${
+      //   count * stringToNumber
+      // } <br />`;
+      // res.innerHTML += `${tab}`;
+      let item = document.createElement("option");
+      item.text = `${stringToNumber} x ${count} = ${count * stringToNumber}`;
+      tab.appendChild(item);
     }
+    //  let count = 1;
+    //  while (count <=10) {
+    //   count++
+    // }
   }
 }
