27e68d2de91e28a8ae7cdd1587935d1785e3bd7b
Isascaraber <isa.escaraber@gmail.com>
2023-11-25 18:49:05 -0300

    Feat: Add exercise 15 about composite varible

--- ModuloF/aula15/ambient.js
+++ ModuloF/aula15/ambient.js
@@ -1,4 +1,6 @@
-let num = [5, 8, 4, 9, 3];
-
-// console.log(num);
-console.log(`Our vector is the ${num}`);
+let num = [5, 8, 2, 9, 3];
+num.push(1); // If I add this command after the sort command, the value 1 will add to the last position, as it will first be organized and then added
+num.sort();
+console.log(num);
+console.log(`Our vector has ${num.length} positions`);
+console.log(`The first value of the vector is ${num[0]}`);
---
+++ ModuloF/aula15/vector.js
@@ -0,0 +1,23 @@
+let values = [8, 1, 7, 4, 2, 9];
+values.sort();
+// console.log(values)
+/*
+console.log(values[0])
+console.log(values[1])
+console.log(values[2])
+console.log(values[3])
+console.log(values[4])
+console.log(values[5])
+*/
+
+//console.log(values);
+/*
+for(let pos = 0; pos < values.length; pos++) {
+  console.log(`The position ${pos} has the value ${values[pos]}`);
+}
+*/
+
+//console.log(values);
+for (let pos in values) {
+  console.log(`The position ${pos} has the value ${values[pos]}`);
+}
