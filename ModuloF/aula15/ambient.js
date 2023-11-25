31bc0b811e860ddc7f4f23eb9038847a1dd4fa5c
Isascaraber <isa.escaraber@gmail.com>
2023-11-25 18:56:46 -0300

    Feat: Add exercise 15 about composite varible

--- ModuloF/aula15/ambient.js
+++ ModuloF/aula15/ambient.js
@@ -4,3 +4,10 @@ num.sort();
 console.log(num);
 console.log(`Our vector has ${num.length} positions`);
 console.log(`The first value of the vector is ${num[0]}`);
+
+let pos = num.indexOf(8); //Where is the value 8? what position it is?
+if (pos == -1) {
+  console.log(`The value was not found`);
+} else {
+  console.log(`The value 8 is the position ${pos}`);
+}
