064931b5aeba8f94d21d05ea068fa33d61d3c8ad
Isascaraber <isa.escaraber@gmail.com>
2023-11-30 06:55:33 -0300

    Feat: add more info about ex 16 function03

--- ModuloF/aula16/function04.js
+++ ModuloF/aula16/function04.js
@@ -5,8 +5,16 @@
 function factorial(n) {
   let fact = 1;
   for (let c = n; c > 1; c--) {
-    //meu contador ira iniciar em um numero e enquanto esse numero for menor que 1 perderá 1.
+    //meu contador ira iniciar em um numero e enquanto esse numero for maior que 1, perderá 1. (5 4 3 2 1)
     fact *= c;
+    //fact = fact * 5
+    //fact = 1 * 5
+    //fact = 5
+    //fact = 1 * 5    5
+    //fact = 5 * 4    20
+    //fact = 20 * 3   60
+    //fact = 60 * 2   120
+    //fact = 120 * 1  120
   }
   return fact;
 }
