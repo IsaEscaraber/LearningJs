7dd90fa3f6e077e810a9e282e0d90918015af8cf
Isascaraber <isa.escaraber@gmail.com>
2023-11-30 22:05:02 -0300

    feat: add new modal about function

--- ModuloF/aula16/ex18/script.js
+++ ModuloF/aula16/ex18/script.js
@@ -1,1 +1,18 @@
-function analyzeNum() {}
+function analyzeNum() {
+  let inputBetween = document.getElementById("numberBetween");
+  let boxValue = document.getElementById("boxAnalyze");
+  let detailBelow = document.getElementById("detail");
+
+  if (
+    inputBetween.value > 100 ||
+    inputBetween.value < 1 ||
+    inputBetween.value.length == 0
+  ) {
+    window.alert("ERRO,");
+  } else {
+    let stringToNumber = Number(inputBetween.value);
+    let values = document.createElement("option");
+    values.text = `Value ${inputBetween} added`;
+    boxValue.appendChild(values);
+  }
+}
