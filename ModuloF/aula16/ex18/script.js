23c5bdfa954b587f4834b1e19697d0fb0c3b7e44
Isascaraber <isa.escaraber@gmail.com>
2023-12-05 14:49:54 -0300

    feat: add new exercice about analyze numbers

--- ModuloF/aula16/ex18/script.js
+++ ModuloF/aula16/ex18/script.js
@@ -1,9 +1,11 @@
-let valueArray = [];
 let inputValue = document.getElementById("numberBetween");
-function analyzeNum() {
-  let boxValue = document.getElementById("boxAnalyze");
+let boxValue = document.querySelector("select#boxAnalyze");
+let detailBelow = document.querySelector("div#detail");
+let valueArray = [];
 
+function analyzeNum() {
   if (
+    isNum(inputValue.value) && !inList(inputValue.value)
     inputValue.value > 100 ||
     inputValue.value < 1 ||
     inputValue.value.length == 0
@@ -14,7 +16,7 @@ function analyzeNum() {
       valueArray.push(inputValue.value);
       for (let pos = inputValue; pos != valueArray.value; pos++) {
         let values = document.createElement("option");
-        values.text = `Value ${valueArray[pos]} added`;
+        values.text = `Value ${valueArray} added`;
         boxValue.appendChild(values);
         return;
       }
@@ -23,7 +25,6 @@ function analyzeNum() {
   }
 }
 function finishValue() {
-  let detailBelow = document.getElementById("detail");
   let boxValue = document.getElementById("boxAnalyze");
   if (valueArray.length == 0) {
     window.alert("ERRO, it is empty!");
