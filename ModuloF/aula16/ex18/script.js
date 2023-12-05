48fdf328c56236e787d4a91bab17b1518aec5fd5
Isascaraber <isa.escaraber@gmail.com>
2023-12-04 21:06:34 -0300

    feat: add new exercice about analyze numbers

--- ModuloF/aula16/ex18/script.js
+++ ModuloF/aula16/ex18/script.js
@@ -2,29 +2,32 @@ let valueArray = [];
 function analyzeNum() {
   let inputValue = document.getElementById("numberBetween");
   let boxValue = document.getElementById("boxAnalyze");
-  let detailBelow = document.getElementById("detail");
 
   if (
     inputValue.value > 100 ||
     inputValue.value < 1 ||
     inputValue.value.length == 0
   ) {
-    window.alert("ERRO,");
+    window.alert("ERRO, incorret value!");
   } else {
     function addValue() {
       valueArray.push(inputValue.value);
+
       let values = document.createElement("option");
-      values.text = `Value ${inputValue.value} added`;
+      values.text = `Value ${valueArray} added`;
       boxValue.appendChild(values);
+      return;
     }
     addValue();
   }
 }
 function finishValue() {
+  let detailBelow = document.getElementById("detail");
   let boxValue = document.getElementById("boxAnalyze");
   if (valueArray.value.length == 0) {
-    window.alert("ERRO,");
+    window.alert("ERRO, it is empty!");
   } else {
-    values.innerHTML = `It will show all the information related to the above content`;
+    window.alert("ok,");
+    detail.innerHTML = `It will show all the information related to the above content`;
   }
 }
